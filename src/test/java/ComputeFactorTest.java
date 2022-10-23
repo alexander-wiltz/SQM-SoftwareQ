af0717122c26491bae8d5e6b2feff3cf77f677a1
alwi1002 <alwi1002@stud.hs-kl.de>
2022-10-23 11:42:32 +0200

    added planned tests

--- src/test/java/ComputeFactorTest.java
+++ src/test/java/ComputeFactorTest.java
@@ -24,4 +24,72 @@ public class ComputeFactorTest {
         assertThat(factors.getFactors()).isEqualTo(expected);
     }
 
+    @Test
+    public void computeTestWithA13() {
+        Long number = (long) 13;
+
+        ComputeFactors factors = new ComputeFactors(number);
+        factors.compute();
+
+        ArrayList<Long> expected = new ArrayList<>();
+        expected.add((long) 1);
+        expected.add((long) 13);
+
+        assertThat(factors.getFactors()).isEqualTo(expected);
+    }
+
+    @Test
+    public void computeTestWithANegative5() {
+        Long number = (long) -5;
+
+        ComputeFactors factors = new ComputeFactors(number);
+        factors.compute();
+
+        ArrayList<Long> expected = new ArrayList<>();
+
+        assertThat(factors.getFactors()).isEqualTo(expected);
+    }
+
+    @Test
+    public void computeTestWithAZero() {
+        Long number = (long) 0;
+
+        ComputeFactors factors = new ComputeFactors(number);
+        factors.compute();
+
+        ArrayList<Long> expected = new ArrayList<>();
+
+        assertThat(factors.getFactors()).isEqualTo(expected);
+    }
+
+    @Test
+    public void computeTestWithADecimal() {
+        Long number = (long) 12.1;
+
+        ComputeFactors factors = new ComputeFactors((long)number);
+        factors.compute();
+
+        ArrayList<Long> expected = new ArrayList<>();
+        expected.add((long) 1);
+        expected.add((long) 2);
+        expected.add((long) 3);
+        expected.add((long) 4);
+        expected.add((long) 6);
+        expected.add((long) 12);
+
+        assertThat(factors.getFactors()).isEqualTo(expected);
+    }
+
+    @Test
+    public void computeTestWithAString() {
+        Long number = Long.parseLong("abc");
+
+        ComputeFactors factors = new ComputeFactors(number);
+        factors.compute();
+
+        ArrayList<Long> expected = new ArrayList<>();
+
+        assertThat(factors.getFactors()).isEqualTo(expected);
+    }
+
 }
