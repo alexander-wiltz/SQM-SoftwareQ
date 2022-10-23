ad4aa1f71c94b9524f328f9a197da729b8196027
alwi1002 <alwi1002@stud.hs-kl.de>
2022-10-23 10:17:10 +0200

    optimized method for computing

--- src/main/java/Factors/ComputeFactors.java
+++ src/main/java/Factors/ComputeFactors.java
@@ -3,47 +3,76 @@ package Factors;
 import java.util.ArrayList;
 
 /**
- * Berechnung aller Teiler einer ganzen Zahl. 
- *
+ * Berechnung aller Teiler einer ganzen Zahl.
+ * <p>
  * Aufgabenstellung
+ * <p>
+ * edit:
+ *
+ * @author Alexander Wiltz
+ * Code dahingehend angepasst, dass auch die Teiler 1 und die Zahl selbst berücksichtigt werden.
  */
 public class ComputeFactors {
 
-	private Long number;
-	private ArrayList<Long> factors;
-	
-	public ComputeFactors() {
-		this.number = (long)2;
-		this.factors = new  ArrayList<Long>();
-	}
+    private final Long number;
+    private final ArrayList<Long> factors;
 
-	public ComputeFactors(Long number) {
-		this.number = number;
-		this.factors = new  ArrayList<Long>();
-	}
-	
-	public ComputeFactors(long number) {
-		this.number = number;
-		this.factors = new  ArrayList<Long>();
-	}
-	
-	public void compute() {
-		Long helpDiv = (long)2;
-		
-		while (helpDiv <= this.number-1) {
-			if (this.number % helpDiv == 0) {
-				// Die Zahl helpDiv ist ein Teiler
-				this.factors.add(helpDiv);
-			}
-			helpDiv++;
-		}
-	}
-	
-	@Override
-        public String toString() {
-	    String text="";
-	    text = "Die Teiler der Zahl " + this.number + ":\n";
-	    text += this.factors.toString();
-	    return text;
-	}
+    private int loopCounter = 0;
+
+    public ComputeFactors() {
+        this.number = (long) 1;
+        this.factors = new ArrayList<>();
+    }
+
+    public ComputeFactors(Long number) {
+        this.number = number;
+        this.factors = new ArrayList<>();
+    }
+
+    public ComputeFactors(long number) {
+        this.number = number;
+        this.factors = new ArrayList<>();
+    }
+
+    public void compute() {
+        Long helpDiv = (long) 2;
+
+        this.factors.add((long) 1);
+        while (helpDiv <= this.number - 1) {
+            if (this.number % helpDiv == 0) {
+                // Die Zahl helpDiv ist ein Teiler
+                this.factors.add(helpDiv);
+            }
+            helpDiv++;
+            this.loopCounter++;
+        }
+        this.factors.add(this.number);
+    }
+
+    public void optimizedCompute() {
+        Long helpDiv = (long) 2;
+
+        this.factors.add((long) 1);
+        while (helpDiv <= (this.number / 2)) {
+            if (this.number % helpDiv == 0) {
+                this.factors.add(helpDiv);
+            }
+            helpDiv++;
+            this.loopCounter++;
+        }
+
+        this.factors.add(this.number);
+    }
+
+    @Override
+    public String toString() {
+        String text = "Die Teiler der Zahl " + this.number + ":\n";
+        text += this.factors.toString();
+        return text;
+    }
+
+    public int getLoopCounter() {
+        return loopCounter;
+    }
+
 }
--- src/main/java/Factors/FactorsMain.java
+++ src/main/java/Factors/FactorsMain.java
@@ -5,12 +5,21 @@ package Factors;
  */
 public class FactorsMain {
 
-	public static void main(String[] args) {
-		
-		Long number = (long)12;
-		ComputeFactors factors = new ComputeFactors(number);
-		factors.compute();
-		
-		System.out.println(factors);
-	}
+    public static void main(String[] args) {
+
+        Long number = (long) 13;
+        ComputeFactors factors = new ComputeFactors(number);
+        factors.compute();
+
+        System.out.println(factors);
+		System.out.println("Schleifendurchlaeufe: " + factors.getLoopCounter());
+
+        // optimized version
+        System.out.println("**************");
+        ComputeFactors factorsOpt = new ComputeFactors(number);
+        factorsOpt.optimizedCompute();
+
+        System.out.println(factorsOpt);
+        System.out.println("Schleifendurchlaeufe: " + factorsOpt.getLoopCounter());
+    }
 }
